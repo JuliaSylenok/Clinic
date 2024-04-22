ae510df46294c9a3a4886fd0df480cd1eeb75077
Julia Sylenok <silenokjulia1@gmail.com>
2024-04-22 18:43:56 +0300

    commit

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,9 +12,7 @@ using WindowsFormsApp2.UserControls;
 
 namespace WindowsFormsApp2
 {
-
-
-    public partial class Form1 : Form
+        public partial class Form1 : Form
     {
         public Form1()
         {
