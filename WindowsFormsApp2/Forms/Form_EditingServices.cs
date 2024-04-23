5bf9caef4836801e8b974dd0633038eb9ecaf0f7
Julia Sylenok <silenokjulia1@gmail.com>
2024-04-23 17:14:12 +0300

    Admin.ViewServices

--- WindowsFormsApp2/Forms/Form_EditingServices.cs
+++ WindowsFormsApp2/Forms/Form_EditingServices.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp2.Classes;
 
 namespace WindowsFormsApp2.Forms
 {
@@ -15,7 +16,28 @@ namespace WindowsFormsApp2.Forms
         public Form_EditingServices()
         {
             InitializeComponent();
+            PopulateDataGridView();
         }
+        private void PopulateDataGridView()
+        {
+            
+            dataGridView_Services.Rows.Clear();
+
+            dataGridView_Services.Columns.Add("Category", "Категорія");
+            dataGridView_Services.Columns.Add("Description", "Опис послуги");
+            dataGridView_Services.Columns.Add("Price", "Ціна");
+
+            foreach (var service in Clinic.Instance.Services)
+            {
+                string[] rowData = {
+                    service.Name,
+                    service.Description,
+                    service.Price.ToString()
+                };
+                dataGridView_Services.Rows.Add(rowData);
+            }
+        }
+
 
         private void btnClose_Click(object sender, EventArgs e)
         {
