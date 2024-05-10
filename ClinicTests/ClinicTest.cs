8d7e47b5a2158c655551f15eca61dceb2a5ebb7d
Julia Sylenok <silenokjulia1@gmail.com>
2024-05-10 20:30:13 +0300

    correction4

--- ClinicTests/ClinicTest.cs
+++ ClinicTests/ClinicTest.cs
@@ -14,9 +14,9 @@ namespace ClinicTests
             clinic.AddService(new Service { Name = "Pediatrics", Description = "Child healthcare services", Price = 80 });
             clinic.AddService(new Service { Name = "Dermatology", Description = "Skin care and treatment", Price = 120 });
 
-            string expected = "Назва: Dentistry, Опис: Dental checkup and treatment, Ціна: 100\r\n" +
-                              "Назва: Pediatrics, Опис: Child healthcare services, Ціна: 80\r\n" +
-                              "Назва: Dermatology, Опис: Skin care and treatment, Ціна: 120\r\n";
+            string expected = "Dentistry,Dental checkup and treatment,100\r\n" +
+                              "Pediatrics,Child healthcare services,80\r\n" +
+                              "Dermatology,Skin care and treatment,120\r\n";
 
             // Act
             string actual = clinic.PrintServices();
